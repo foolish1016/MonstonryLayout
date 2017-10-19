9eb61b5f9c5311a84ea1b686a522f26558b619cc
孙亚男 <yanan.sun@lianbi.com.cn>
2017-10-19 16:54:08 +0800

    Initial Commit

---
+++ TestImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestImage
+//
+//  Created by 孙亚男 on 2017/10/19.
+//  Copyright © 2017年 sam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
